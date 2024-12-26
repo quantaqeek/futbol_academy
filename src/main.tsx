3be446e38d49a7ec3df48763693d91cb77b5bd47
Bettenoch <bettenoch254@gmail.com>
2024-12-26 11:02:42 +0300

    Create main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { RouterProvider, createRouter } from "@tanstack/react-router"
+import React from "react"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import { routeTree } from "./routeTree.gen"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { Provider } from "./components/ui/provider.tsx"
+
+// OpenAPI.BASE = "https://sil-backend-assessment-wwkj.onrender.com"
+// OpenAPI.TOKEN = async () => {
+// 	return localStorage.getItem("access_token") || ""
+// }
+const queryClient = new QueryClient()
+const router = createRouter({ routeTree })
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router
+	}
+}
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<Provider>
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
+		</Provider>
+	</StrictMode>,
 )
