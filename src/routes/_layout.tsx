536edb18f6bb1fb0c0dfb8e70055839a4b8ce3ba
Bettenoch <bettenoch254@gmail.com>
2024-12-26 11:00:59 +0300

    CReate ,main appss layout

---
+++ src/routes/_layout.tsx
@@ -0,0 +1,14 @@
+import { Flex } from "@chakra-ui/react";
+import { Outlet, createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/_layout")({
+  component: Layout,
+});
+
+function Layout() {
+  return (
+    <Flex maxW="large" direction={"column"} h="auto" position="relative">
+      <Outlet />
+    </Flex>
+  );
+}
