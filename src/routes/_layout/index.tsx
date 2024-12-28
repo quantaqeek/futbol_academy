cd43eb8758272bb256afd92600b36ae8afc09d76
Bettenoch <bettenoch254@gmail.com>
2024-12-28 10:43:01 +0300

    ADD bg image

--- src/routes/_layout/index.tsx
+++ src/routes/_layout/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Text } from "@chakra-ui/react";
+import { Box, Container, Heading, Text } from "@chakra-ui/react";
 import { createFileRoute } from "@tanstack/react-router";
 import { ColorModeButton } from "../../components/ui/color-mode";
 
@@ -9,13 +9,22 @@ export const Route = createFileRoute("/_layout/")({
 function Dashboard() {
   return (
     <>
-      <Container maxW="full">
-        <Box pt={12} m={4}>
+      <Container maxW="full" p={0} m={0}  bg={`url("https://img.freepik.com/free-vector/futuristic-city-other-planet-megapolis_1441-2735.jpg?uid=R133883599&ga=GA1.1.1379508871.1718173539&semt=ais_hybrid")`}
+          backgroundPosition={"center"}
+          backgroundSize={"cover"}
+          backgroundRepeat={"no-repeat"}>
+        <Box
+          pt={24}
+          m={4}
+          minH={"100vh"}
+    
+        >
           <Text fontSize="2xl">Hi, 👋🏼</Text>
           <Text>Welcome back, nice to see you again!</Text>
           <ColorModeButton />
           <Text>Where is this?</Text>
         </Box>
+      
       </Container>
     </>
   );
