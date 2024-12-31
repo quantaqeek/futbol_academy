ea8ad9988f44740c79b04d3131ad2fc59f57f499
Bettenoch <bettenoch254@gmail.com>
2024-12-31 13:32:08 +0300

    Create data

---
+++ src/data/home/index.ts
@@ -0,0 +1,30 @@
+export const highlights = [
+    {
+        id: 1,
+        name: "Soccer",
+        type: "Football",
+        image: "/images/home/play1.jpg",
+        link: "",
+    },
+    {
+        id: 2,
+        name: "Futsal",
+        type: "Football",
+        image: "/images/home/play2.jpg",
+        link: "",
+    },
+    {
+        id: 3,
+        name: "Touch Football",
+        type: "Football",
+        image: "/images/home/play3.jpg",
+        link: "",
+    },
+    {
+        id: 4,
+        name: "Street Football",
+        type: "Football",
+        image: "/images/home/play4.jpg",
+        link: "",
+    },
+]
